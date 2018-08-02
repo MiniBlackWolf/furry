e0a8a098f192d09d48d8ca0d6256a2200ea0db48
lbWC <h610177609>
2018-08-02 17:47:03 +0800

    2018/8/2

--- app/src/main/java/com/example/wolf/delivery/delivery.java
+++ app/src/main/java/com/example/wolf/delivery/delivery.java
@@ -1,24 +1,61 @@
 package com.example.wolf.delivery;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.View;
 
 import com.example.wolf.R;
 
 import org.xutils.view.annotation.ContentView;
-import org.xutils.x;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 @ContentView(R.layout.delivery)
-public class delivery extends AppCompatActivity{
+public class delivery extends AppCompatActivity {
+    @BindView(R.id.vv1)
+    View vv1;
+    float toff;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        x.view().inject(this);
+        setContentView(R.layout.delivery);
+        ButterKnife.bind(this);
+        Display defaultDisplay = getWindowManager().getDefaultDisplay();
+        int width = defaultDisplay.getWidth();
+        toff = (float) (width / 2.2);
+
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        overridePendingTransition(0,0);
+    }
+
+    @OnClick({R.id.xuandifanhui, R.id.pick2, R.id.d1})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.xuandifanhui:
+                finish();
+                break;
+            case R.id.pick2:
+                if(vv1.getTranslationX()==toff){
+                    return;
+                }
+                ObjectAnimator translationX = ObjectAnimator.ofFloat(vv1, "translationX", vv1.getTranslationX(), toff);
+                translationX.setDuration(500);
+                translationX.start();
+                break;
+            case R.id.d1:
+                ObjectAnimator translationXx = ObjectAnimator.ofFloat(vv1,"translationX",vv1.getTranslationX(),0f);
+                translationXx.setDuration(500);
+                translationXx.start();
+                break;
+        }
     }
 }
